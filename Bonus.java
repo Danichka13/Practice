3e18ce13ee1b1b7dc7129ac897fc671f4c344ea8
Danichka13 <99400335+Danichka13@users.noreply.github.com>
2022-02-13 19:48:07 +0400

    bonus

--- Bonus.java
+++ Bonus.java
@@ -6,8 +6,8 @@ public class Bonus {
     }
 
     public static void twoOfFour(){
-        boolean b_one, b_two, b_three, b_four;
-        boolean[] bln = {b_one = true, b_two = false, b_three = false, b_four = true};
+        boolean b_one = true, b_two = false, b_three = false, b_four = true;
+        boolean[] bln = {b_one, b_two, b_three, b_four};
         int count = 0;
         for (int i = 0; i < bln.length; i++){
             if(bln[i]){
